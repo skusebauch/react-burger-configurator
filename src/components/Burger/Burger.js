cd280cc6635d47baef4786ee96b72fa93859beb4
Sven Kusebauch <sven.kusebauch@gmail.com>
2020-08-31 17:56:30 +0200

    transfrom state object into array - burger component

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,13 +3,21 @@ import classes from "./Burger.css";
 import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 
 const burger = (props) => {
-  const transformedIngredients = Object.keys(props.ingredients).map(
-    (ingrKey) => {
+  // transfrom state object into array
+  let transformedIngredients = Object.keys(props.ingredients)
+    .map((ingrKey) => {
       return [...Array(props.ingredients[ingrKey])].map((_, index) => {
         return <BurgerIngredient key={ingrKey + index} type={ingrKey} />;
       });
-    }
-  );
+    })
+    .reduce((prevArray, currentArray) => {
+      return prevArray.concat(currentArray);
+    }, []);
+  // check if no ingredients, display h1
+  if (transformedIngredients.length === 0) {
+    transformedIngredients = <h1>Please start adding ingredients</h1>;
+  }
+
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top" />
