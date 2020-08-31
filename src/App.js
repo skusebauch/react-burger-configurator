a94ffabf02e446d78495a6d6c3174dcd9adfc64b
Sven Kusebauch <sven.kusebauch@gmail.com>
2020-08-31 13:43:55 +0200

    setup layout component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React, { Component } from "react";
+import Layout from "./components/Layout/Layout";
 
 class App extends Component {
   render() {
-    return <div>This is just a test</div>;
+    return (
+      <div>
+        <Layout>
+          <p>Test</p>
+        </Layout>
+      </div>
+    );
   }
 }
 
