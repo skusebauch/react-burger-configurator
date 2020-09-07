45104b58603971ccd1921597affccbbef2a228e8
Sven Kusebauch <sven.kusebauch@gmail.com>
2020-09-07 19:36:06 +0200

    add axios and setup firebase to post order to db

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,7 @@ import Burger from "../../components/Burger/Burger";
 import BuildControls from "../../components/Burger/BuildControls/BuildControls";
 import Modal from "../../components/UI/Modal/Modal";
 import OrderSummary from "../../components/Burger/OrderSummary/OrderSummary";
+import axios from "../../axios-order";
 
 const INGREDIENT_PRICES = {
   salad: 0.5,
@@ -78,7 +79,26 @@ class BurgerBuilder extends Component {
   };
 
   continueOrderNowHandler = () => {
-    alert("You continue!!");
+    //alert("You continue!!");
+    const order = {
+      ingredients: this.state.ingredients,
+      // in real project re-calculate price at server side
+      price: this.state.totalPrice,
+      customer: {
+        name: "Sven Kusebauch",
+        address: {
+          street: "Teststreet 1",
+          zipCode: "41324",
+          country: "Germany",
+        },
+        email: "test@test.com",
+      },
+      deliveryOptions: "fastest",
+    };
+    axios
+      .post("/orders.json", order)
+      .then((response) => console.log(response))
+      .catch((error) => console.log(error));
   };
 
   render() {
