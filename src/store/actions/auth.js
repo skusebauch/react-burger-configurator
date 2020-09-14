e9857473fce9861b29514516c50ee2672fb231a7
Sven Kusebauch <sven.kusebauch@gmail.com>
2020-09-14 18:50:46 +0200

    getting token from backend

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,3 +1,4 @@
+import axios from "axios";
 import * as actionTypes from "./actionTypes";
 
 export const authStart = () => {
@@ -24,5 +25,23 @@ export const auth = (email, password) => {
   // redux thunk
   return (dispatch) => {
     dispatch(authStart());
+    const authData = {
+      email: email,
+      password: password,
+      returnSecureToken: true,
+    };
+    axios
+      .post(
+        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyA1Y2xKyfPKlXhFz0lPzH4diUFBg2IhiHw",
+        authData
+      )
+      .then((response) => {
+        console.log(response);
+        dispatch(authSuccess(response.data));
+      })
+      .catch((err) => {
+        console.log(err);
+        dispatch(authFail(err));
+      });
   };
 };
