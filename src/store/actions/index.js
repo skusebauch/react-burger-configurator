689bf6cdd931b8b61d3775752395d51a2a41fbaf
Sven Kusebauch <sven.kusebauch@gmail.com>
2020-09-15 14:57:05 +0200

    fixed connect and routing error

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -4,4 +4,4 @@ export {
   initIngredients,
 } from "./burgerBuilder";
 export { purchaseBurger, purchaseInit, fetchOrders } from "./order";
-export { auth, logout, setAuthRedirect } from "./auth";
+export { auth, logout, setAuthRedirect, authCheckState } from "./auth";
