b51f132f7e9e9f4c315e1ec5910057c95a6e88fc
Sven Kusebauch <sven.kusebauch@gmail.com>
2020-09-16 11:03:22 +0200

    test burgerbuilder container

--- src/components/Navigation/NavigationItems/NavigationItems.test.js
+++ src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -19,4 +19,10 @@ describe("<NavigatoinsItems />", () => {
     wrapper.setProps({ isAuthenticated: true });
     expect(wrapper.find(NavigationItem)).toHaveLength(3);
   });
+  it("should render logout <NavigationItem /> element if authenticated", () => {
+    wrapper.setProps({ isAuthenticated: true });
+    expect(
+      wrapper.contains(<NavigationItem link="/logout">Logout</NavigationItem>)
+    ).toEqual(true);
+  });
 });
